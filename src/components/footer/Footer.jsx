39b3dea08211db10a1e50fd97526026e360a49c1
Nguyen Tuananh <nguyentuananh@Nguyens-MacBook-Air.local>
2023-04-26 01:01:18 +0700

    add home

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 
+import "./footer.scss";
+
+import { Link } from "react-router-dom";
+
+import bg from "../../assets/footer2.jpeg";
+import logo from "../../assets/logo/logo.png";
+import "./footer.scss";
 const Footer = () => {
-  return <div>Footer</div>;
+    return (
+        <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                        <Link to="/">tMovies</Link>
+                    </div>
+                </div>
+            </div>
+            <div className="footer__copyright">
+                <Link to="/">&copy; 2023 Nguyễn Tuấn Anh</Link>
+            </div>
+        </div>
+    );
 };
+
 export default Footer;
